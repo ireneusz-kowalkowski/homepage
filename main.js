ce0b4650042bb28a175904cde61fb44b82ef14cb
Ireneusz Kowalkowski <irasovv@gmail.com>
2020-05-14 17:44:45 +0200

    js added to project

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'iReneusz';
+const age = 34;
+
+ console.log(firstName);
+  console.log(age);
+  console.log('Piona');
+  console.log(`Mam na imię ${firstName} i mam ${age} lata.`)
